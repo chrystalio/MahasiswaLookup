034a51b0f6807aeef8740f4a8ff7ad67c496efe5
Chrystalio <chrystalio.k@outlook.com>
2024-03-18 23:12:04 +0700

    Update mockup

--- src/routes/Mockup.svelte
+++ src/routes/Mockup.svelte
@@ -1,6 +1,6 @@
 <div class="mockup-code text-xs lg:text-md py-10 lg:text-lg">
     <pre data-prefix="$"><code>Welcome to MahasiswaLookup!</code></pre> 
-    <pre data-prefix="$" class="text-error"><code>Please wait, project in progress...</code></pre>
+    <pre data-prefix=">"><code>Please wait, project in progress...</code></pre>
     <pre data-prefix=">" class="text-warning"><code>Installing...</code></pre> 
-    <pre data-prefix=">" class="text-success"><code>Done!</code></pre>
+    <pre data-prefix=">" class="text-error"><code>Status: [NOT READY]!</code></pre>
 </div>
