6dcc8c494cc6f40bc9a8a2c6201942f40d9dde29
Chrystalio <chrystalio.k@outlook.com>
2024-03-19 20:40:51 +0700

    Fix padding issue on mobile dev

--- src/components/SearchForm.svelte
+++ src/components/SearchForm.svelte
@@ -8,6 +8,6 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
     </label>
     <div class="container">
-        <button class="btn btn-wide px-44 md:px-96 my-5">Submit</button>
+        <button class="btn btn-wide px-40 md:px-96 my-5">Submit</button>
     </div>
 </form>
